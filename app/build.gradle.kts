9395cc7b27babe85e6aefdfb17d919bcf26a1583
Marmel <marksilla12@gmail.com>
2026-09-15 15:25:19 +0800

    Update build.gradle.kts for Java and Kotlin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.marksilla.myaiagent"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.marksilla.myaiagent"
         minSdk = 26
@@ -12,7 +14,17 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
